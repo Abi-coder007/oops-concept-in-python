eb7dce066c45fee119c6f669d36deb977fc4bd3e
Abi-coder007 <abishekcoder2007@gmail.com>
2026-03-23 21:26:35 +0530

    Add files via upload

---
+++ Decorator.py
@@ -0,0 +1,20 @@
+def my_decorator(func):
+    def wrapper():
+        print("Something before the function runs")
+        func()
+        print("Something after the function runs")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+
+'''
+op/
+Something before the function runs
+Hello!
+Something after the function runs
+'''
