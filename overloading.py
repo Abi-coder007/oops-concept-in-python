431f7549784d12c34ffd42ea4096fb041dbeede8
Abi-coder007 <abishekcoder2007@gmail.com>
2026-03-26 21:33:26 +0530

    Add files via upload

---
+++ overloading.py
@@ -0,0 +1,43 @@
+# class animal:
+#     def speak(self):
+#         print("Animal")
+#
+# class dog(animal):
+#     def speak(self):
+#         print("Dog")
+#
+#
+# class cat(animal):
+#     def speak(self):
+#         print("Cat")
+#
+# a = animal()
+# d = dog()
+# c = cat()
+#
+# a.speak()
+# d.speak()
+# c.speak()
+
+'''
+
+o/p
+Animal
+Dog
+Cat
+'''
+
+
+
+class tree:
+    def say(self):
+        print("i am tree")
+
+t = tree()
+t.say()
+
+'''
+o/p
+i am tree
+
+'''
