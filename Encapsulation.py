9fee302c1fa4c08f7fb8c6fb4c1c0e1d863db42e
Abi-coder007 <abishekcoder2007@gmail.com>
2026-03-30 22:57:41 +0530

    Add files via upload

---
+++ Encapsulation.py
@@ -0,0 +1,26 @@
+# class BankAccount:
+#     def __init__(self, balance):
+#         self.__balance = balance
+#
+#     def deposit(self, amount):
+#         self.__balance += amount
+#
+#     def get_balance(self):
+#         return self.__balance
+#
+
+
+
+#
+# class Person:
+#     def __init__(self):
+#         self.name = "John"
+#
+# p = Person()
+# print(p.name)
+
+'''
+o/p
+John
+
+'''
