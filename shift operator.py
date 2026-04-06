07d95306eacb9129f5b9923a67286b667e99edfc
Abi-coder007 <abishekcoder2007@gmail.com>
2026-04-06 22:45:31 +0530

    Add files via upload

---
+++ shift operator.py
@@ -0,0 +1,9 @@
+# Shift Left Example
+a = 5
+left_shift = a << 1
+print("Left Shift Result:", left_shift)
+
+# Shift Right Example
+b = 8
+right_shift = b >> 1
+print("Right Shift Result:", right_shift)
